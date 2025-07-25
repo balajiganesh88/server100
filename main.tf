79abe00533a7b6cd3586a588d9a17f285bec0049
balajiganesh88 <balaji.ganesh1988@gmail.com>
2025-07-26 00:31:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,32 +14,3 @@ resource "aws_vpc" "vpc200" {
         Name = "vpc200"
     }
 }
-resource "aws_subnet" "publicsubnet1" {
-    cidr_block = "172.17.0.0/24"  
-    vpc_id = "aws_vpc.vpc200.id"
-    tags = {
-        Name = "publicsubnet1"
-    }
-}
-resource "aws_subnet" "publicsubnet2" {
-    cidr_block = "172.17.1.0/24"
-    vpc_id = "aws_vpc.vpc200.id"
-    tags = {
-        Name = "publicsubnet2"
-    }
-}
-resource "aws_subnet" "privatesubnet1" {
-    cidr_block = "172.17.2.0/24"
-    vpc_id = "aws_vpc.vpc200.id"
-    tags = {
-        Name = "privatesubnet1"
-    }
-}
-resource "aws_subnet" "privatesubnet2" {
-    cidr_block = "172.17.3.0/24"
-    vpc_id = "aws_vpc.vpc200_vpc.id"
-    tags = {
-        Name = "privatesubnet2"
-    }
-}
-
