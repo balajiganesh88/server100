1dec15216209fabc075c520c63b2d04d507bb716
balajiganesh88 <balaji.ganesh1988@gmail.com>
2025-07-26 00:35:02 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,25 +1,25 @@
-resource "aws_subnet" "publicsubnet1" {
+resource "aws_subnet" "publicsubnet100" {
     cidr_block = "172.17.0.0/24"  
     vpc_id = "aws_vpc.vpc200.id"
     tags = {
         Name = "publicsubnet1"
     }
 }
-resource "aws_subnet" "publicsubnet2" {
+resource "aws_subnet" "publicsubnet200" {
     cidr_block = "172.17.1.0/24"
     vpc_id = "aws_vpc.vpc200.id"
     tags = {
         Name = "publicsubnet2"
     }
 }
-resource "aws_subnet" "privatesubnet1" {
+resource "aws_subnet" "privatesubnet100" {
     cidr_block = "172.17.2.0/24"
     vpc_id = "aws_vpc.vpc200.id"
     tags = {
         Name = "privatesubnet1"
     }
 }
-resource "aws_subnet" "privatesubnet2" {
+resource "aws_subnet" "privatesubnet200" {
     cidr_block = "172.17.3.0/24"
     vpc_id = "aws_vpc.vpc200_vpc.id"
     tags = {
