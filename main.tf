8a62f605775c7bfc940d3e648adfdaca5ddee685
balajiganesh88 <balaji.ganesh1988@gmail.com>
2025-07-25 13:12:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ provider "aws" {
     access_key = "AKIAQUOGQXX7BTPLZXNC"
     secret_key = "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"
 }
-resource "AWS_VPC" "VPC100" {
+resource "aws_VPC" "VPC100" {
     cidr_block = "172.17.0.0/16"
     tags =  {
         Name = "vpc100"
@@ -17,28 +17,28 @@ resource "AWS_VPC" "VPC100" {
 }
 resource "aws_subnet" "publicsubnet1" {
     cidr_block = "172.17.0.0/24"  
-    vpc_id = "AWS_VPC.VPC100.id"
+    vpc_id = "aws_VPC.VPC100.id"
     tags = {
         Name = "publicsubnet1"
     }
 }
 resource "aws_subnet" "publicsubnet2" {
     cidr_block = "172.17.1.0/24"
-    vpc_id = "AWS_VPC.VPC100.id"
+    vpc_id = "aws_VPC.VPC100.id"
     tags = {
         Name = "publicsubnet2"
     }
 }
 resource "aws_subnet" "privatesubnet1" {
     cidr_block = "172.17.2.0/24"
-    vpc_id = "AWS_VPC.VPC100.id"
+    vpc_id = "aws_VPC.VPC100.id"
     tags = {
         Name = "privatesubnet1"
     }
 }
 resource "aws_subnet" "privatesubnet2" {
     cidr_block = "172.17.3.0/24"
-    vpc_id = "AWS_VPC.VPC100.id"
+    vpc_id = "aws_VPC.VPC100.id"
     tags = {
         Name = "privatesubnet2"
     }
