0c45b4649804804a5388e799ce79afb08b250e65
balajiganesh88 <balaji.ganesh1988@gmail.com>
2025-07-25 13:46:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ provider "aws" {
     access_key = "AKIAQUOGQXX7BTPLZXNC"
     secret_key = "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"
 }
-resource "aws_VPC" "VPC100" {
+resource "aws_vpc" "VPC100" {
     cidr_block = "172.17.0.0/16"
     tags =  {
         Name = "vpc100"
