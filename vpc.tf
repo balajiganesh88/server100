4a4207f6ec854443e7a119bd6619cc39a5e27176
balajiganesh88 <balaji.ganesh1988@gmail.com>
2025-07-25 16:26:30 +0530

    updated region and vpc
    updated region and vpc

--- vpc.tf
+++ vpc.tf
@@ -5,7 +5,7 @@ terraform {
   }
 }
 provider "aws" {
-    region = us-east-1 
+    region = "us-east-1"
     access_key = "AKIAQUOGQXX7BTPLZXNC"
     secret_key = "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"
 }
@@ -13,7 +13,7 @@ resource "aws_vpc" "vpc200" {
     cidr_block = "172.17.0.0/16"
     tags =  {
         Name = "vpc100"
-        }
+    }
 }
 resource "aws_subnet" "publicsubnet1" {
     cidr_block = "172.17.0.0/24"  
