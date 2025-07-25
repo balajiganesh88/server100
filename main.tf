b988ad127a90533e9f78c76d6b85f8ce4f73a599
balajiganesh88 <balaji.ganesh1988@gmail.com>
2025-07-25 23:43:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    aws = {source = "hashicorp/aws"
+    version = "~> 6.5"}
+  }
+}
+provider "aws" {
+    region = "us-east-1" 
+ 
+}
+
 resource "aws_vpc" "vpc200" {
     cidr_block = "172.17.0.0/16"
     tags =  {
