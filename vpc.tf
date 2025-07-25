7d5c6a805ffe288273e031c9fe4a67a9a8e5d231
balajiganesh88 <balaji.ganesh1988@gmail.com>
2025-07-25 23:08:20 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -10,38 +10,3 @@ provider "aws" {
     secret_key = "AWS_SECRET_ACCESS_KEY"
 }
 
-resource "aws_vpc" "vpc200" {
-    cidr_block = "172.17.0.0/16"
-    tags =  {
-        Name = "vpc100"
-    }
-}
-resource "aws_subnet" "publicsubnet1" {
-    cidr_block = "172.17.0.0/24"  
-    vpc_id = "aws_vpc.vpc200.id"
-    tags = {
-        Name = "publicsubnet1"
-    }
-}
-resource "aws_subnet" "publicsubnet2" {
-    cidr_block = "172.17.1.0/24"
-    vpc_id = "aws_vpc.vpc200.id"
-    tags = {
-        Name = "publicsubnet2"
-    }
-}
-resource "aws_subnet" "privatesubnet1" {
-    cidr_block = "172.17.2.0/24"
-    vpc_id = "aws_vpc.vpc200.id"
-    tags = {
-        Name = "privatesubnet1"
-    }
-}
-resource "aws_subnet" "privatesubnet2" {
-    cidr_block = "172.17.3.0/24"
-    vpc_id = "aws_vpc.vpc200.id"
-    tags = {
-        Name = "privatesubnet2"
-    }
-}
-
