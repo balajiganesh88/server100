37487c0e8d01417b571580fa14e5aadb192c88a8
balajiganesh88 <balaji.ganesh1988@gmail.com>
2025-07-25 23:37:58 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -6,7 +6,7 @@ terraform {
 }
 provider "aws" {
     region = "us-east-1" 
-    access_key = "AKIAQUOGQXX7CKA3X36O"
-    secret_key = "aNSTUsldWnhHeIQNc8gxNirAKDVOh/W6cheb+uh"
+    access_key = "AWS_ACCESS_KEY_ID"
+    secret_key = "AWS_SECRET_ACCESS_KEY"
 }
 
